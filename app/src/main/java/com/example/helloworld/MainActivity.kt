cbebdfa8e9cca66437a38ce40b70739cedefa6b6
sd-unl <maximum20250302@gmail.com>
2026-02-21 15:56:05 +0200

    Update via JSON builder

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.view.Gravity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val textView = TextView(this).apply {
+            text = "Hello, World!"
+            textSize = 24f
+            gravity = Gravity.CENTER
+        }
+        
+        setContentView(textView)
+    }
+}
